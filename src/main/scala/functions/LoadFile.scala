bd000f8e1de65d1249f09deff8b8fe7e3c5e3bc1
mvchernov <mvchernov@phoenixit.ru>
2023-01-28 14:28:10 +0300

    initTables

--- src/main/scala/functions/LoadFile.scala
+++ src/main/scala/functions/LoadFile.scala
@@ -4,12 +4,13 @@ import org.apache.spark.sql.{DataFrame, SparkSession}
 import org.apache.spark.sql.types.StructType
 
 object LoadFile {
-  def load(spark: SparkSession, pathTable: String, schemaTable: StructType): DataFrame = {
+  def load(name: String, pathTable: String, schemaTable: StructType)(implicit spark: SparkSession): Unit = {
     spark.read
       .options(Map("delimiter" -> "\\t",
       "dateFormat" -> "yyyy-MM-dd"))
       .schema(schemaTable)
       .csv(pathTable)
+      .createOrReplaceTempView(name)
   }
 
 }
